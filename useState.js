28fe3d3196e6d424a7d9f91825fee06fc01df7fe
Srinivase1204@ <55191480+srinivas98765@users.noreply.github.com>
2026-05-08 07:10:51 +0530

    Update useState.js

--- useState.js
+++ useState.js
@@ -3,9 +3,9 @@ import './App.css';
 import { use, useState } from 'react';
 
 const App = () => {
-  const array=useState(0);
-  const counter=array[0];
-  const setCounter=array[1];
+                                                                      //const array=useState(0);
+  const [counter, setCounter] = useState(0);                          //const counter=array[0];
+                                                                     // const setCounter=array[1];
   function increase(){
     setCounter(counter+1);
   }
