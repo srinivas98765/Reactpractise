182d75edca223df54fb75914d4f1176d70d61ac2
Srinivase1204@ <55191480+srinivas98765@users.noreply.github.com>
2026-05-08 07:28:08 +0530

    Add counter functionality to App component

--- useState.js
+++ useState.js
@@ -23,3 +23,27 @@ const App = () => {
 }
 
 export default App;
+
+import logo from './logo.svg';
+import './App.css';
+import { use, useState } from 'react';
+
+const App = () => {
+  const [details, setDetails] = useState({counter:0,name:"Counter"});
+  function increase(){
+    setDetails({...details, counter: details.counter + 1});
+  }
+  return(
+    <div>
+      <input 
+        type="text" 
+        onChange={(e)=>e.target.value}
+      />
+      <h1>Counter : {details.counter}</h1>
+      <button onClick={increase}>Increase</button>
+    </div>
+
+  );
+}
+
+export default App;
