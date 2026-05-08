d8c7cec375c5f5db206f69d1cd548b0e784524d5
Srinivase1204@ <55191480+srinivas98765@users.noreply.github.com>
2026-05-08 07:07:32 +0530

    Implement counter using useState

---
+++ useState.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { use, useState } from 'react';
+
+const App = () => {
+  const array=useState(0);
+  const counter=array[0];
+  const setCounter=array[1];
+  function increase(){
+    setCounter(counter+1);
+  }
+  return(
+    <div>
+      <h1>Counter : {counter}</h1>
+      <button onClick={increase}>Increase</button>
+    </div>
+
+  );
+}
+
+export default App;
